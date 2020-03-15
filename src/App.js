44abcdae38a3737cf9e12f27e8acff972bb7b19a
achandi <chandi.ajit@gmail.com>
2020-03-14 21:57:33 -0400

    hosting subdirectories on the server app.js BrowserRouter example

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // is server has a baseUrl with domain + /something/,
+      // need to set basename prop for BrowserRouter
+      //<BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
